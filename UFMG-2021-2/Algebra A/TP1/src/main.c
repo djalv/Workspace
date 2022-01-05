ae8106261fa8ad46bd453dd22a2a266de2edf238
djalv009-ClearLinuxPC <djalvinho009@hotmail.com>
2022-01-05 01:55:40 -0300

    Inicio do TP1 Algebra A

--- UFMG-2021-2/Algebra A/TP1/src/main.c
+++ UFMG-2021-2/Algebra A/TP1/src/main.c
@@ -5,5 +5,13 @@ int main() {
     mpz_t m, n;
     mpz_inits(m, n, NULL);
 
+    gmp_scanf("%Zd %Zd", m, n);
 
+    mpz_t resp;
+    mpz_init(resp);
+    mpz_mul(resp, m, n);
+
+    gmp_printf("%Zd\n", resp);
+
+    mpz_clears(m, n, resp, NULL);
 }
