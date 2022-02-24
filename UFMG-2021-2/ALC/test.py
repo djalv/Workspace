d34637f37685e42bed9ef1fdd9d2a3074131ead1
djalv009-linux <djalvinho009@hotmail.com>
2022-02-24 20:54:21 -0300

    jj

--- UFMG-2021-2/ALC/test.py
+++ UFMG-2021-2/ALC/test.py
@@ -1,3 +1,16 @@
+from cmath import sqrt
 import numpy as np
 
-man = np.array()
+father = np.array([-0.5, 0.87])
+mother = np.array([-1, 0])
+g = np.array([-0.71, 0.71])
+
+
+u = (mother + father)/2
+
+ub = np.inner(u, g) * g
+y = u - ub
+
+z = sqrt(1 - ((np.linalg.norm(y))**2))
+Va = y - z*g
+print(z*g)
--- UFMG-2021-2/Algebra A/extendedEuclides.py
+++ UFMG-2021-2/Algebra A/extendedEuclides.py
@@ -8,4 +8,5 @@ def euclides(a,b):
 
     return (g,y,x-q*y)
 
-print(euclides(14,17))
+print(euclides(5,216))
+
