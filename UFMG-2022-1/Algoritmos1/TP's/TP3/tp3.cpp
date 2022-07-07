83a046407e105198ae3b300a8ff8543bda94ba68
djalv <alvaroneto98@protonmail.com>
2022-07-07 02:35:34 -0300

    Update tp3.cpp

--- UFMG-2022-1/Algoritmos1/TP's/TP3/tp3.cpp
+++ UFMG-2022-1/Algoritmos1/TP's/TP3/tp3.cpp
@@ -5,99 +5,151 @@
 
 using namespace std;
 
-class Solution {
-public:
-	vector<int> NSL(vector<int> heights){ // Function to find indices of next smallest left element
-        vector<int> left;
-        stack<pair<int,int>> st;
-        for(int i=0;i<heights.size();i++){
-            if(st.empty())
-                left.push_back(-1);
-            else if(!st.empty() && st.top().first < heights[i])
-                left.push_back(st.top().second);
-            else if(!st.empty() && st.top().first >= heights[i]){
-                while(!st.empty() && st.top().first>=heights[i])
-                    st.pop();
-                if(st.empty())
-                    left.push_back(-1);
-                else 
-                    left.push_back(st.top().second);
+typedef struct {
+    int x;
+    int y;
+    int area;
+}Area, Table;
+
+
+vector <int> leftIndex(vector <int> &arr) { // O(n)
+    vector <int> l; // Indices no qual é o elemento mais proximo a esquerda com valor menor q i
+    
+    stack <pair <int, int>> s; // Stack para memorizar os elementos menores anteriores em ordem crescente dos valores
+
+    for(int i = 0; i < arr.size(); i++) {
+        if(s.empty()) { // Se a stack estiver vazia é pq nn exite nenhum elemento a esquerda menor q i
+            l.push_back(-1);
+        }
+        else if(!s.empty() && s.top().first < arr[i]) { // Se o valor do topo da stack for menor q o valor de i
+            l.push_back(s.top().second);                // então o indice do topo da stack é o 
+        }                                               // elemento mais proximo a esquerda menor q o valor de i
+
+        else if(!s.empty() && s.top().first >= arr[i]) { // Se o valor do topo da stack for maior q o valor de i
+            while(!s.empty() && s.top().first >= arr[i]) { // então desempilhamos até acharmos um valor menor q o valor de i
+                s.pop();
             }
-            st.push({heights[i],i});
-        }
-        return left;
-    }
-    vector<int> NSR(vector<int> heights){  // Function to find indices of next smallest right element
-        vector<int> right;
-        stack<pair<int,int>> st;
-        for(int i=heights.size()-1;i>=0;i--){
-            if(st.empty())
-                right.push_back(heights.size());
-            else if(!st.empty() && st.top().first<heights[i])
-                right.push_back(st.top().second);
-            else if(!st.empty() && st.top().first>=heights[i]){
-                while(!st.empty() && st.top().first>=heights[i])
-                    st.pop();
-                if(st.empty())
-                    right.push_back(heights.size());
-                else 
-                    right.push_back(st.top().second);
+            if(s.empty()) { // Se a stack esvazia
+                l.push_back(-1); // é pq não existe elemento com valor menor q o valor de i
             }
-            st.push({heights[i],i});
-        }
-        reverse(right.begin(),right.end());
-        return right;
-    }
-    int MAH(vector<int>& heights) {  //Function to find maximum space of histogram
-        vector<int> right;
-        vector<int> left;
-        
-        right = NSR(heights);
-        left = NSL(heights);
-        
-        vector<int> width;
-        int mx = 0;
-        for(int i=0;i<left.size();i++){
-            width.push_back(right[i]-left[i]-1);
-        }
-        
-        for(int i=0;i<heights.size();i++){
-            mx=max(mx,heights[i]*width[i]);
-        }
-        
-        return mx;
-    }
-    int maximalRectangle(vector<vector<char>>& matrix) {  // Finally......our required Maximal Rectangle Function
-        int n=matrix.size();
-        if(n == 0) return 0;
-        
-        int m=matrix[0].size();
-        vector<int> v;
-        for(int j=0;j<m;j++){
-            v.push_back(matrix[0][j]-'0');
-        }
-        int mx = MAH(v);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]=='0')
-                    v[j]=0;
-                else
-                    v[j]=v[j]+(matrix[i][j]-'0');
+            else { // Senão o elemento do topo da stack é o elemento mais proximo a esquerda menor q o valor de i
+                l.push_back(s.top().second);
             }
-            mx = max(mx,MAH(v));
         }
-        return mx;
+        s.push({arr[i], i});
     }
-};
+    return l;
+}
+
+vector <int> rightIndex(vector <int> &arr) { // O(n)
+    vector <int> r; // Indices no qual é o elemento mais proximo a direita com valor menor q i
+    stack <pair <int, int>> s; // Stack para memorizar os elementos menores posteriores em ordem crescente dos valores
+
+    for(int i = arr.size()-1; i >= 0; i--) {
+        if(s.empty()) { // Se a stack estiver vazia é pq não existe elemento menor q i a direita
+            r.push_back(arr.size());
+        }
+        else if(!s.empty() && s.top().first < arr[i]) { // Se o valor do topo da stack for menor q o valor de i
+            r.push_back(s.top().second);                // então o indice do topo da stack é o 
+        }                                               // elemento mais proximo a esquerda menor q o valor de i
+        
+        else if(!s.empty() && s.top().first >= arr[i]) { // Se o valor do topo da stack for maior q o valor de i
+            while(!s.empty() && s.top().first >= arr[i]) { // então desempilhamos até acharmos um valor menor q o valor de i
+                s.pop();
+            }
+            if(s.empty()) { // Se a stack esvazia
+                r.push_back(arr.size()); // é pq não existe elemento com valor menor q o valor de i
+            }
+            else { // Senão o elemento do topo da stack é o elemento mais proximo a direita menor q o valor de i
+                r.push_back(s.top().second);
+            }
+        }
+        s.push({arr[i], i});
+    }
+    reverse(r.begin(), r.end());
+    return r;
+}
+
+int maxAreaRectangle(vector <int> &arr, int &x, int &y) {
+    int maxArea = 0, area = 0;
+
+    vector <int> l = leftIndex(arr);
+    vector <int> r = rightIndex(arr);
+
+    vector <int> width;
+
+    for(int i = 0; i < arr.size(); i++) {
+        width.push_back(r[i] - l[i] - 1);
+    }
+
+    maxArea = width[0] * arr[0];
+    x = width[0];
+    y = arr[0];
+
+    for(int i = 0; i < arr.size(); i++) {
+        area = width[i] * arr[i];
+        if(area >= maxArea) {
+            if(area == maxArea) {
+                if(width[i] > y) {
+                    x = width[i];
+                    y = arr[i];
+                }
+            }
+            else {
+                x = width[i];
+                y = arr[i];
+            }
+            maxArea = area;
+        }
+    }
+    
+    return maxArea;
+}
+
+void areaOfRectangles(vector <vector <int>> &arr, vector <Area> &areas) {
+    vector <int> v(arr[0].size(), 0);
+    Area a, maxArea;
+    int area, x, y, spaceLength, spaceWidth;
+    
+    maxArea.x = -1;
+    maxArea.y = -1;
+    maxArea.area = -1;
+
+    for(int i = 0; i < arr.size(); i++) {
+        for(int j = 0; j < arr[i].size(); j++) {
+            if(arr[i][j] == 0) {
+                v[j] = 0;
+            }
+            else {
+                v[j] += arr[i][j];
+            }
+        }
+        area = maxAreaRectangle(v, spaceLength, spaceWidth);
+        a.area = area;
+        a.x = spaceLength;
+        a.y = spaceWidth;
+        areas.push_back(a);
+
+        if(a.area > maxArea.area) {
+            maxArea = a;
+        }
+    }
+}
 
 int main() {
-    int rows, columns, qtdTables, x, y;
-    int area, spaceArea, maxTableArea = 0, maxWidth;
+    int rows, columns, qtdTables, c, x, y, spaceLength, spaceWidth;
+    int area, maxWidth;
     int tableX, tableY;
-    char in, c;
+    char in;
+    Table t, maxTableArea;
     
-    Solution s;
-    vector <vector <char>> space;
+    maxTableArea.area = -1;
+    maxTableArea.x = -1;
+    maxTableArea.y = -1;
+    
+    vector <vector <int>> space;
+    vector <Area> spaceAreas;
+    vector <Table> tables;
 
     cin >> rows >> columns;
     space.resize(rows);
@@ -106,41 +158,47 @@ int main() {
         for(int j = 0; j < columns; j++) {
             cin >> in;
             if(in == '#') {
-                c = '0';
+                c = 0;
             }
             else if(in == '.') {
-                c = '1';
+                c = 1;
             }
             space[i].push_back(c);
         }
         in = ' ';
-        c = ' ';
     }
 
-    spaceArea = s.maximalRectangle(space);
+    areaOfRectangles(space, spaceAreas);
     cin >> qtdTables;
-
+    
     for(int i = 0; i < qtdTables; i++) {
         cin >> x >> y;
-        area = x * y;
-        
-        if(area <= spaceArea){
-            if(area == maxTableArea) {
-                if(y > tableY) {
-                    tableX = x;
-                    tableY = y;;
-                    maxTableArea = area;
+        t.x = x;
+        t.y = y;
+        t.area = x * y;
+        tables.push_back(t);
+
+    }
+    
+    for(int i = 0; i < tables.size(); i++) {
+        for(int j = 0; j < spaceAreas.size(); j++) {
+            if(tables[i].area <= spaceAreas[j].area) {
+                if((tables[i].x <= spaceAreas[j].x && tables[i].y <= spaceAreas[j].y) || (tables[i].x <= spaceAreas[j].y && tables[i].y <= spaceAreas[j].x)) {
+                    if(tables[i].area >= maxTableArea.area) {
+                        if(tables[i].area == maxTableArea.area) {
+                            if(tables[i].y > maxTableArea.y) {
+                                maxTableArea = tables[i];
+                            }
+                        }
+                        else {
+                            maxTableArea = tables[i];
+                        }
+                    }
                 }
-            }
-            else if(area > maxTableArea) {
-                tableX = x;
-                tableY = y;;
-                maxTableArea = area;
             }
         }
     }
     
-    cout << tableX << " " << tableY << endl;
-    
+    cout << maxTableArea.x << " " << maxTableArea.y << endl;
     return 0;
 }
